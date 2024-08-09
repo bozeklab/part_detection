bcf5c1e52ba9eb636366a7123044565124f3c3ff
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-10 00:19:52 +0200

    added mitodataset

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 import torch
 import torch.multiprocessing
 from torch.utils.data import Dataset
+import torchvision
 import torchvision.transforms.v2 as transforms
 from torchvision.models import resnet101, ResNet101_Weights
 import json
