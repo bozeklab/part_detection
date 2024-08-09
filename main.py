c90d29d6b5963cd271be7b5ae3512d530899ba94
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-10 01:03:25 +0200

    added mitodataset

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import json
 from torch.utils.tensorboard import SummaryWriter
 from train import train, validation
 from datasets import TrivialAugmentWideNoColor
+from datasets import MitoFolder
 
 def main():
     parser = argparse.ArgumentParser(description='PDiscoNet')
